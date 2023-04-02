a0f72758deb19135dfdc88526a80636ed2e7ae8d
sethu63 <19211a0560@bvrit.ac.in>
2023-04-02 17:42:15 +0530

    -on-init-implemented-for-alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
 
   constructor() {
+    
+  }
+  ngOnInit(): void {
     const buildmessage: string = "This site is under build, fixing minor issues/Responsiveness will be active by: 02/04/2023";
     alert(buildmessage);
   }
