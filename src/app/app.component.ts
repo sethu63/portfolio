a609ccc25f208a3329699601efd4d6200f05d17d
sethu63 <19211a0560@bvrit.ac.in>
2023-04-04 11:50:49 +0530

    -mobile-site-optimised-04/04/23-11/50

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'Sethus portfolio';
+  title = 'Portfolio';
 
   links = [  
     { route: '', name: 'IntroComponent' },  
